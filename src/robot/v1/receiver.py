755925ed1d3891c950f8e3fa24da997b7c84c5c0
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-01-29 14:58:08 -0500

    delay after reset servo positions

--- src/robot/v1/receiver.py
+++ src/robot/v1/receiver.py
@@ -97,9 +97,11 @@ def main():
         # Initialize servos
         controller.initialize_servos()
 
+        # Set all servos to starting positions
         controller.set_servo_positions_to_starting_positions()
+        time.sleep(3)
 
-        # Get current positions of slave servos as their baseline
+        # Get current positions of follower servos as their baseline
         follower_baselines = controller.get_servo_positions(controller.get_follower_ids())
         leader_baselines = None
         buffer = ""
